474e55bf8c704f1d781621c33cd33286ff7bdd25
Muhammad Apu <apu16-334@diu.edu.bd>
2026-08-17 22:34:16 +0600

    Implemented Greeting component by input getting the the name

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
             <Counter />
             <br />
             <Toggle />
+            <br />
             <Greeting />
         </>
     )
--- src/day-8/Greeting.jsx
+++ src/day-8/Greeting.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+
 function Greeting() {
+    const [name, setName] = useState('stranger')
+
     return (
         <>
+            <div>
+                <input type="text" onChange={e => setName(e.target.value)} />
+            </div>
 
+            <p>Hello, {name}!</p>
         </>
     )
 }
