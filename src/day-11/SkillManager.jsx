9a9c3d4f1e48b55480f08adad76658264ff8e5f1
Muhammad Apu <apu16-334@diu.edu.bd>
2026-08-20 18:21:42 +0600

    implemented form for add skill

--- src/day-11/SkillManager.jsx
+++ src/day-11/SkillManager.jsx
@@ -2,12 +2,46 @@ import { useState } from "react"
 
 function SkillManager() {
     const [skills, setSkills] = useState([
-        { id: 1, name: 'Backend developer', level: 'advanced' },
-        { id: 2, name: 'Frontend developer', level: 'intermediate' }
+        { id: 1, name: 'Backend developer', level: 'Advanced' },
+        { id: 2, name: 'Frontend developer', level: 'Intermediate' }
     ])
 
-    function handleDelete(id) {
+    const [formData, setFormData] = useState({ name: '', level: '' })
 
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setFormData(prev => ({ ...prev, [name]: value }));
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setSkills(prev => [...prev, { ...formData, id: Date.now() }]);
+        setFormData({ name: '', level: '' });
+    }
+
+    function handleDelete(id) {
+        setSkills(prev => prev.filter(skill => skill.id !==id));
+    }
+
+    function handleToggle (id) {
+
+        setSkills(prev => prev.map(skill => {
+            if(skill.id === id) {
+                if(skill.level === 'Beginner') {
+                    return {...skill, level: 'Intermediate'}
+                }
+
+                if(skill.level === 'Intermediate') {
+                    return {...skill, level: 'Advanced'}
+                }
+
+                if(skill.level === 'Advanced') {
+                    return {...skill, level: 'Beginner'}
+                }
+            }else {
+                return skill;
+            }
+        }))
     }
 
     return (
@@ -17,12 +51,31 @@ function SkillManager() {
                 {skills.map(skill => (
 
                     <li key={skill.id}>
-                        Name: {skill.name} Level: {skill.level}
+                        <span style={{ marginRight: '20px' }}>Name: {skill.name}</span>
+                        <span style={{ marginRight: '20px', cursor:'pointer' }} onClick={() => handleToggle(skill.id)}>Level: {skill.level}</span>
                         <button onClick={() => handleDelete(skill.id)}>Delete</button>
                     </li>
 
                 ))}
             </ol>
+
+            <form onSubmit={handleSubmit} >
+                <div>
+                    <label>Enter Skill Name: </label>
+                    <input type="text" name="name" value={formData.name} onChange={handleChange} />
+                </div>
+                <div style={{ marginTop: '10px' }}>
+                    <label>Choose Skill Level:</label>
+                    <select name="level" value={formData.level} onChange={handleChange} >
+                        <option value="">select level</option>
+                        <option value="Beginner">Beginner</option>
+                        <option value="Intermediate">Intermediate</option>
+                        <option value="Advanced">Advanced</option>
+                    </select>
+                </div>
+
+                <div style={{ marginTop: '10px' }}><button type="submit">Add</button></div>
+            </form>
         </>
     )
 }
