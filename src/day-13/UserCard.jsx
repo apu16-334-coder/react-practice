027e4b774c49711c0ec3b27ee5042b619bd89176
Muhammad Apu <apu16-334@diu.edu.bd>
2026-08-21 23:54:36 +0600

    Implemented User directory app with useEffect concept

--- src/day-13/UserCard.jsx
+++ src/day-13/UserCard.jsx
@@ -1,7 +1,10 @@
-function UserCard() {
+function UserCard({users}) {
     return (
         <>
-
+            <ol>
+                {users.map(u => <li key={u.id}>Name: {u.name}. Email: {u.email}. City: {u.address.city}.</li>)}
+            </ol>
+            
         </>
     )
 }
--- src/day-13/UserDirectory.jsx
+++ src/day-13/UserDirectory.jsx
@@ -1,7 +1,58 @@
+import { useState, useEffect } from "react";
+import UserCard from "./UserCard";
+
 function UserDirectory() {
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [query, setQuery] = useState('')
+
+    useEffect(() => {
+        async function fetchUsers(params) {
+            try {
+                setIsLoading(true);
+
+                const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+                if (!response.ok) throw new Error('Failed to fetch users');
+
+                const data = await response.json();
+
+                setUsers(data);
+
+            } catch (error) {
+                setError(error.message)
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchUsers();
+    }, [])
+
+    
+    const filteredUsers = users.filter(u => u.name.toLowerCase().includes(query.trim().toLowerCase()))
+
+    useEffect(() => {
+        document.title = `User Directory — ${filteredUsers.length} users`;
+    }, [filteredUsers])
+
+
+    if(isLoading) return <p>Page is Loading</p>
+    if(error) return <p style={{ color: 'red' }}>Error: {error}</p>
+
+    // console.log(users);
+
     return (
         <>
             <h2>User Directory</h2>
+
+            <h4>Enter user name: </h4>
+            <div>
+                <input type="text" placeholder="search..." value={query} onChange={(e) => setQuery(e.target.value)} />
+            </div>
+
+            <UserCard users = {filteredUsers} />
         </>
     )
 }
